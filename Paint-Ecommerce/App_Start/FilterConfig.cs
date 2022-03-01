80fea39e2876c9522f7379533d0ac76b0769169f
ThembelaniSelby <worldafrika@hotmail.com>
2022-03-01 18:44:43 +0200

    Final

---
+++ Paint-Ecommerce/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Paint_Ecommerce
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
